14f8636d7e2078fc767410f35414f81bd3cf04d9
yyt <dusxo1203@gmail.com>
2022-02-17 20:12:45 +0900

    BOJ

--- 07.문자열/10.그룹 단어 체커.cpp
+++ 07.문자열/10.그룹 단어 체커.cpp
@@ -11,6 +11,16 @@
  관련 블로그: https://dusxo1203.tistory.com/
 */
 
+/*
+ https://www.acmicpc.net/problem/1316
+
+ 문제: 영어 대소문자와 공백으로 이루어진 문자열이 주어진다.
+ 이 문자열에는 몇 개의 단어가 있을까? 이를 구하는 프로그램을 작성하시오.
+ 단, 한 단어가 여러 번 등장하면 등장한 횟수만큼 모두 세어야 한다.
+
+ 관련 블로그: https://dusxo1203.tistory.com/58
+*/
+
 #include <stdio.h>
 #include <string.h>
 #pragma warning (disable:4996)
@@ -20,35 +30,47 @@ int main()
 
     char input[101] = { 0, };
     int WordCnt = 0, cnt = 0, size = 0;
+    int  alphabat[26] = { 0, };
 
     scanf("%d", &WordCnt);
     cnt = WordCnt;
+
     for (int i = 0; i < WordCnt; i++)
     {
+        for (int k = 0; k < 26; k++)
+        {
+            alphabat[k] = 0;
+        }
+
         scanf("%s", &input);
         size = strlen(input);
-
-        for (int j = 0; j < size; j++)
+       
+        for (int i = 0; i < size; i++)
         {
-            for (int k = j+1; k < size; k++)
+            int plus = 0;
+            if (i == 0)
             {
-                if (input[j] == input[k])
+                plus = input[0] - 97;
+                alphabat[plus] += 1;
+            }
+            else
+            {
+                if (input[i] != input[i - 1])
                 {
-                    for (k; k < size; k++)
-                    {
-
-                    }
-                    if (k - j > 1)
-                    {
-                        cnt--;
-                        break;
-                   }
+                    plus = input[i] - 97;
+                    alphabat[plus] += 1;
                 }
             }
-            break;
+        }
+        for (int j = 0; j < 26; j++)
+        {
+            if (alphabat[j] > 1)
+            {
+                cnt--;
+                break;
+            }
         }
     }
     printf("%d", cnt);
-
     return 0;
 }
